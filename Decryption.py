f9753dbb748f5d734a69ff7ec328d18a4948ede6
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-04-20 15:00:47 +0800

    Checking of symbol

--- Decryption.py
+++ Decryption.py
@@ -9,10 +9,22 @@ print("\033[0;32m=" * 80)
 encrypted_text = input("\033[0;32mTYPE ENCRYPTED TEXT: ")
 decrypted_text = ""
 # check every symbol to substitute
+for i in range(len(encrypted_text)):
     # if *, make it a
+    if encrypted_text [i] == "*":
+        decrypted_text += "a"
     # if &, make it e
+    elif encrypted_text [i] == "&":
+        decrypted_text += "e"
     # if &, make it i
+    elif encrypted_text [i] == "#":
+        decrypted_text += "i"
     # if &, make it o
+    elif encrypted_text [i] == "+":
+        decrypted_text += "o"
     # if &, make it u
-
+    elif encrypted_text [i] == "!":
+        decrypted_text += "u"
+    else:
+        decrypted_text += encrypted_text[i]
 # print decrypted text 

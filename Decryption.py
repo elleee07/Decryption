0ebb2b4f6a386e199f17da71f68a94891500f8e8
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-04-20 14:53:01 +0800

    ask the user to input

---
+++ Decryption.py
@@ -0,0 +1,15 @@
+# ELLA MARIE A. MALLARI
+# BSCPE 1-4
+
+#ask user to input their name: 
+name = input("Please enter your name: ")
+
+# input encrypted text
+# check every symbol to substitute
+    # if *, make it a
+    # if &, make it e
+    # if &, make it i
+    # if &, make it o
+    # if &, make it u
+
+# print decrypted text 
